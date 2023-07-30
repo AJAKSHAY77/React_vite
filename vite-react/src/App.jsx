135e04300a0805ef7c0688a1cb49a1bf5ab246dd
akshay77 <ajakshayjain11@gmail.com>
2023-07-30 16:10:47 +0530

    first commit

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Dogcard from './dogcard.jsx'
+
+
+
+function App() {
+
+  return (
+    <>
+     <div>
+      akshay
+      <Dogcard/>
+      <Dogcard/>
+      <Dogcard/>
+     
+     </div>
+    </>
+  )
+}
+
+export default App
